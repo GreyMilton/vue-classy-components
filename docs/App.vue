c8632766788a22c744cc60af38e7a3840cb4cab6
Grey Milton's MBP <grey.milton@gmail.com>
2023-05-17 05:52:15 +0930

    show & style examples in docs

--- docs/App.vue
+++ docs/App.vue
@@ -1,13 +1,29 @@
 <script setup>
-import ClassyButtonMain from '../src/components/ClassyButtonMain.vue';
+import ClassyButtonPrimary from '../src/components/ClassyButtonPrimary.vue';
+import ClassyButtonSecondary from '../src/components/ClassyButtonSecondary.vue';
 </script>
 
 <template>
-  <div>
-    <ClassyButtonMain>BUTTON</ClassyButtonMain>
+  <div class="classy-div">
+    <ClassyButtonSecondary>Cancel</ClassyButtonSecondary>
+    <ClassyButtonPrimary>Submit</ClassyButtonPrimary>
+  </div>
+  <div class="classy-div">
+    <ClassyButtonSecondary state="waiting">Cancel</ClassyButtonSecondary>
+    <ClassyButtonPrimary state="waiting">Submit</ClassyButtonPrimary>
+  </div>
+  <div class="classy-div">
+    <ClassyButtonSecondary state="disabled">Cancel</ClassyButtonSecondary>
+    <ClassyButtonPrimary state="disabled">Submit</ClassyButtonPrimary>
   </div>
 </template>
 
 <style scoped>
 @import './style.css';
+.classy-div {
+  display: flex;
+  gap: 1rem;
+  align-items: start;
+  margin-top: 20px;
+}
 </style>
