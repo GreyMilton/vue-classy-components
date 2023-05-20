2399d5df312053b40d3ddf9e5b0c9fb75ee08456
Grey Milton's MBP <grey.milton@gmail.com>
2023-05-21 07:14:39 +0930

    install & setup vue-styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,28 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const path = require('path');
+
+module.exports = {
+  codeSplit: false,
+  copyCodeButton: true,
+  simpleEditor: false,
+  title: 'Vue Classy Components',
+  styleguideDir: 'docs_styleguidist',
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          exclude: /node_modules/,
+          loader: 'vue-loader',
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [new VueLoaderPlugin()],
+  },
+  components: './src/components/**/*.vue',
+};
